448d2716012e0f7b24970cfac14a85cd2527d400
Daniel Petrovic <info@laddpriskollen.se>
2025-04-20 16:27:27 +0200

    Fix: lade till React-import och rensade dubbletter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
